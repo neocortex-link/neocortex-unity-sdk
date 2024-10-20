6f47c1fed423d1d29e7e8eb6a1ff0d61f41f4d08
Sercan Altundas <srcnalt@users.noreply.github.com>
2024-10-21 01:19:01 +0300

    [init] initial commit

---
+++ Editor/NeocortexSettingsWindow.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using Neocortex.Data;
+
+namespace Neocortex.Editor
+{
+    public class NeocortexSettingsWindow : EditorWindow
+    {
+        private static NeocortexSettings settings;
+
+        [MenuItem("Tools/Neocortex Settings", false, 0)]
+        public static void ShowWindow()
+        {
+            NeocortexSettingsWindow window = GetWindow<NeocortexSettingsWindow>("Neocortex Settings");
+            window.minSize = window.maxSize = new Vector2(512, 124);
+            window.ShowUtility();
+        }
+
+        private void OnEnable()
+        {
+            settings = Resources.Load<NeocortexSettings>("NeocortexSettings");
+
+            if (settings == null)
+            {
+                settings = CreateInstance<NeocortexSettings>();
+                AssetDatabase.CreateAsset(settings, "Assets/Resources/NeocortexSettings.asset");
+                AssetDatabase.SaveAssets();
+            }
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("API Key", EditorStyles.boldLabel);
+            settings.apiKey = EditorGUILayout.PasswordField("Enter API Key", settings.apiKey);
+
+            if (GUILayout.Button("Save API Key"))
+            {
+                EditorUtility.SetDirty(settings);
+                AssetDatabase.SaveAssets();
+            }
+        }
+    }
+}
