7e10700ff5f9133c0f38f776009c813bdef75b7b
Sercan (Sarge) Altundas <srcnalt@users.noreply.github.com>
2025-03-26 12:17:03 +0200

    [feat] mic selection update

--- Runtime/AudioReceiver.cs
+++ Runtime/AudioReceiver.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,6 +5,8 @@ namespace Neocortex
 {
     public abstract class AudioReceiver: MonoBehaviour
     {
+        public const string MIC_INDEX_KEY = "neocortex-mic-index";
+        
         [SerializeField] private bool usePushToTalk;
         public bool UsePushToTalk { get => usePushToTalk; protected set => usePushToTalk = value; }
         public float Amplitude { get; protected set; }
