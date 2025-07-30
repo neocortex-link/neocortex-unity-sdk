d61a4e24ed38c1c68c9caac634829553d70f9f95
Sercan (Sarge) Altundas <srcnalt@users.noreply.github.com>
2025-07-30 18:05:54 +0300

    [feat] content moved into V2 API folder

--- Runtime/API/V2/Services/ApiRequest.cs
+++ Runtime/API/V2/Services/ApiRequest.cs
@@ -11,13 +11,28 @@ namespace Neocortex.API
     {
         private const string BASE_URL = "https://api.neocortex.link/v2";
         private readonly NeocortexSettings settings = Resources.Load<NeocortexSettings>("Neocortex/NeocortexSettings");
+        private readonly JsonSerializerSettings jsonSerializerSettings = new() { NullValueHandling = NullValueHandling.Ignore };
         
         public event Action<string> OnTranscriptionReceived;
         public event Action<AudioClip> OnAudioResponseReceived;
         public event Action<ChatResponse> OnChatResponseReceived;
         public event Action<string> OnRequestFailed; 
+        public event Action<Message[]> OnChatHistoryReceived; 
         
         private string message;
+
+        private void SetHeaders()
+        {
+            if (settings == null || string.IsNullOrEmpty(settings.apiKey))
+            {
+                throw new Exception("API Key is required. Please add it in the Tools > Neocortex > API Key Setup.");
+            }
+            
+            Headers = new Dictionary<string, string>()
+            {
+                { "x-api-key", settings.apiKey }
+            };
+        }
         
         public async void Send<TInput, TOutput>(string characterId, TInput input)
         {
@@ -27,16 +42,8 @@ namespace Neocortex.API
                 {
                     throw new Exception("Project ID is required");
                 }
-                
-                if (settings == null || string.IsNullOrEmpty(settings.apiKey))
-                {
-                    throw new Exception("API Key is required. Please add it in the Tools > Neocortex > API Key Setup.");
-                }
 
-                Headers = new Dictionary<string, string>()
-                {
-                    { "x-api-key", settings.apiKey }
-                };
+                SetHeaders();
             
                 // here transcription request
                 if (typeof(TInput) == typeof(AudioClip))
@@ -86,10 +93,7 @@ namespace Neocortex.API
                     };
             
                     UnityWebRequest request = await Send(payload);
-                    ApiResponse response = JsonConvert.DeserializeObject<ApiResponse>(request.downloadHandler.text, new JsonSerializerSettings
-                    {
-                        NullValueHandling = NullValueHandling.Ignore
-                    });
+                    ApiResponse response = JsonConvert.DeserializeObject<ApiResponse>(request.downloadHandler.text, jsonSerializerSettings);
 
                     PlayerPrefs.SetString("neocortex-session-id", response.sessionId);
                     
@@ -122,6 +126,37 @@ namespace Neocortex.API
                     
                     OnAudioResponseReceived?.Invoke(audioClip);
                 }
+            }
+            catch (Exception e)
+            {
+                OnRequestFailed?.Invoke(e.Message);
+                Debug.LogError(e.Message);
+            }
+        }
+
+        public async void GetChatHistory(int limit = 10)
+        {
+            try
+            {
+                SetHeaders();
+                
+                var data = new
+                {
+                    limit,
+                    sessionId = PlayerPrefs.GetString("neocortex-session-id", ""),
+                };
+
+                ApiPayload payload = new ApiPayload()
+                {
+                    url = $"{BASE_URL}/chat/session",
+                    data = GetBytes(data),
+                    responseType = ApiResponseType.Text
+                };
+
+                UnityWebRequest request = await Send(payload);
+                ApiChatHistory response = JsonConvert.DeserializeObject<ApiChatHistory>(request.downloadHandler.text, jsonSerializerSettings);
+
+                OnChatHistoryReceived?.Invoke(response.messages);
             }
             catch (Exception e)
             {
