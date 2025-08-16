72a1b94d3a9d007dc1ce794221ab8f3e88651ce9
Sercan (Sarge) Altundas <srcnalt@users.noreply.github.com>
2025-08-16 16:41:10 +0300

    [feat] emotion integration

---
+++ Runtime/Data/Enums/Emotions.cs
@@ -0,0 +1,26 @@
+namespace Neocortex.Data
+{
+  public enum Emotions
+  {
+    Neutral,
+    Happy,
+    Pleased,
+    Disappointed,
+    Upset,
+    
+    Amazed,
+    Curious,
+    Confused,
+    Alarmed,
+
+    Fascinated,
+    Impressed,
+    Annoyed,
+    Angry,
+
+    Confident,
+    Reassured,
+    Concerned,
+    Scared,
+  }
+}
